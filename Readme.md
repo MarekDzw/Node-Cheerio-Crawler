87df336b77a48eae9bbab20d54d40d2f43528e5a
Marek Dzwonnik <mardzw7@gmail.com>
2018-11-29 20:12:53 +0100

    End :(

--- Readme.md
+++ Readme.md
@@ -1,4 +1,4 @@
-##Simple web crawler to get data from website
+## Simple web crawler to get data from website
 
 ### In this small project i used only 2 dependencies:
 
@@ -6,3 +6,5 @@
 - npm install cheerio
 
 App is pretty simply to use. Clone or fork then clone this repo. Open terminal in root folder of this project and type "npm install", to see how it works run "node server.js" command in your terminal.
+
+### To use this app just fork or clone this repo and inside root folder run "npm install" and then "node server.js"
--- server.js
+++ server.js
@@ -5,14 +5,13 @@ request(
   'https://www.otodom.pl/sprzedaz/mieszkanie/kielce/?search%5Bdist%5D=0&search%5Bsubregion_id%5D=450&search%5Bcity_id%5D=181',
   function(error, response, html) {
     if (!error && response.statusCode == 200) {
-      var $ = cheerio.load(html);
+      let $ = cheerio.load(html);
       $('h3').each(function(i, element) {
-        var a = $(this);
-        var url = a.children('a').attr('href');
+        let a = $(this);
+        let url = a.children('a').attr('href');
         if (url == undefined) {
           return;
         }
-        console.log(url);
         getSingle(url);
       });
     }
@@ -22,25 +21,25 @@ request(
 function getSingle(url) {
   request(url, function(error, response, html) {
     if (!error && response.statusCode == 200) {
-      var $ = cheerio.load(html);
+      let $ = cheerio.load(html);
 
-      var title = $('.section-offer-title h1').text();
-      var description = $('.text-contents')
+      let title = $('.section-offer-title h1').text();
+      let description = $('.text-contents')
         .children('div')
         .children('p')
         .text();
-      var price = $('.param_price span strong').text();
-      var size = $('.param_m span strong').text();
-      var floor = $('.param_floor_no span strong').text();
-      var img = $('.section-offer-gallery img').attr('src');
-      var id = $('.col-md-offer-content')
+      let price = $('.param_price span strong').text();
+      let size = $('.param_m span strong').text();
+      let floor = $('.param_floor_no span strong').text();
+      let img = $('.section-offer-gallery img').attr('src');
+      let id = $('.col-md-offer-content')
         .children('.text-details')
         .children('.left')
         .children('p')
         .first()
         .text()
         .substr(20, 8);
-      var offer = {
+      let offer = {
         title: title,
         description: description,
         price: price,
